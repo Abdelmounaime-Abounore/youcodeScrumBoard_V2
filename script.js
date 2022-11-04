2ed1fc9160617d135dc0c8d20f49980f418d2d75
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2022-11-04 10:13:06 +0100

    last push

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-console.log("lddz");
-
-var taskTitle = document.getElementById('task_title');
-   // var taskPriority = document.getElementById(task_priority);
+   var taskTitle = document.getElementById('task_title');
+   var taskPriority = document.getElementById('task_priority');
    // var taskStatut = document.getElementById(task_statut);
    // var taskDate = document.getElementById(task_date);
-   // var taskDescription = document.getElementById(task_description);
-   console.log(taskTitle)
+   // var taskDescription = document.getElementById(task_description)
 
 function editTask(id){
    // document.getElementById("task-id").value = id;
    taskTitle.value = document.getElementById(id +'titel_Id').getAttribute('titles');
-   console.log(document.getElementById(id +'titel_Id'));
+   taskPriority = document.getElementById(id + 'priority_Id').getAttribute('priority'); 
+   
 
 }
 
