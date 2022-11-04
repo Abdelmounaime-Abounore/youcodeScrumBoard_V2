e1c90295b1e013513f59186734af51109ba7dd24
Abdelmounaime-Abounore <112935116+Abdelmounaime-Abounore@users.noreply.github.com>
2022-11-04 10:14:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
    var taskTitle = document.getElementById('task_title');
-   var taskPriority = document.getElementById('task_priority');
    // var taskStatut = document.getElementById(task_statut);
    // var taskDate = document.getElementById(task_date);
    // var taskDescription = document.getElementById(task_description)
@@ -7,7 +6,7 @@
 function editTask(id){
    // document.getElementById("task-id").value = id;
    taskTitle.value = document.getElementById(id +'titel_Id').getAttribute('titles');
-   taskPriority = document.getElementById(id + 'priority_Id').getAttribute('priority'); 
+  
    
 
 }
